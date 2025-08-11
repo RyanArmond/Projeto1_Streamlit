9253e7a8a75fe30b0d4eeceb052275e1a9008822
Ryan Armond Airoso <ryanarmond@gmail.com>
2025-08-11 10:20:48 -0300

    UPDATED pages

--- pages/Consulta.py
+++ pages/Consulta.py
@@ -1,43 +1,9 @@
 import streamlit as st
 import pandas as pd
 
+dados = pd.read_csv("clientes.csv")
 
-st.set_page_config(
-    'Consulta de Cadastros',
-    page_icon="padaria.png",
-    layout='wide',
-)
-
-
-pessoas_cadastradas = pd.read_csv("clientes.csv")
-
-
-st.title('Consulta ao Cadastro Padarias Iguaçu 🍞')
+st.title("Clientes cadastrados")
 st.divider()
 
-
-st.header("Pessoas Cadastradas:")
-st.dataframe(
-    pessoas_cadastradas,
-    hide_index=True,
-    column_config={
-    'nome': st.column_config.TextColumn(
-        "Nome_Cliente",
-        help='Nome dos Clientes Cadastrados',
-    ),
-    
-    'data_nascimento': st.column_config.TextColumn(
-        'Data de Nascimento',
-        help='A data de nascimento dos clientes'
-    ),
-    
-    'tipo_pessoa': st.column_config.TextColumn(
-        'Tipo do Cliente',
-        help='(Pessoa física/Pessoa jurídica)'
-    )
-    
-    },
-    width=1300             
-)
-
-
+st.dataframe(dados)
